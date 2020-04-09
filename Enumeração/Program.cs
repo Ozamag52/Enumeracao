b4844216c4a51b17ffb8574a6caedb9563171fc6
Gustavo Peixoto <henriquegustavo2010@gmail.com>
2020-04-09 12:22:30 -0300

    Criação da entidade order e enumeração de status do pedido

---
+++ Enumeração/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Enumeração.Entities;
+namespace Enumeração
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order();
+            order.Id = 1080;
+            order.Moment = DateTime.Now;
+            order.Status = Entities.Enums.OrderStatus.PendingPayment;
+            Console.WriteLine(order);
+        }
+
+        
+    }
+}
